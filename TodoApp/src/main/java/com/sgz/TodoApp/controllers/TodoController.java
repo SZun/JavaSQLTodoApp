3e79fcf325376f54d3932fd47aa3de2512a3aca8
Zun <zunsamuel97@gmail.com>
2020-07-23 15:35:20 -0500

    refactored package naming

--- TodoApp/src/main/java/com/sgz/TodoApp/controllers/TodoController.java
+++ TodoApp/src/main/java/com/sgz/TodoApp/controllers/TodoController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sgz.TodoApp.entities.controllers;
+package com.sgz.TodoApp.controllers;
 
 import com.sgz.TodoApp.entities.Todo;
 import com.sgz.TodoApp.exceptions.InvalidEntityException;
