0485497d272081de66d1fe94187c021d44d06533
Zun <zunsamuel97@gmail.com>
2020-07-23 11:30:16 -0500

    removed un-used imports

--- TodoApp/src/main/java/com/sgz/TodoApp/App.java
+++ TodoApp/src/main/java/com/sgz/TodoApp/App.java
@@ -1,6 +1,5 @@
 package com.sgz.TodoApp;
 
-import com.sgz.TodoApp.entities.Todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- TodoApp/src/main/java/com/sgz/TodoApp/entities/Todo.java
+++ TodoApp/src/main/java/com/sgz/TodoApp/entities/Todo.java
@@ -5,7 +5,6 @@
  */
 package com.sgz.TodoApp.entities;
 
-import java.lang.reflect.Field;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
