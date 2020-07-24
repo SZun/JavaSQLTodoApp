33f2cf3007e459d882c31eb74d75343ced42e61f
Zun <zunsamuel97@gmail.com>
2020-07-23 21:17:49 -0500

    removed cross origin annotation in controller

--- TodoApp/src/main/java/com/sgz/TodoApp/controllers/TodoController.java
+++ TodoApp/src/main/java/com/sgz/TodoApp/controllers/TodoController.java
@@ -15,7 +15,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author samg.zun
  */
-@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders="*")
 @RestController
 @RequestMapping("/api")
 public class TodoController {
@@ -37,7 +35,6 @@ public class TodoController {
     @Autowired
     TodoService service;
     
-    @CrossOrigin
     @GetMapping("/todos")
     public ResponseEntity<List<Todo>> getAll() throws NoItemsException{
         return ResponseEntity.ok(service.getAll());
