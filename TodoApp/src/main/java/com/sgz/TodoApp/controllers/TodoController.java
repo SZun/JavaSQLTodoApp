339136e84ac3781feb1e86d533b75b305a3133a0
Zun <zunsamuel97@gmail.com>
2020-07-26 15:36:21 -0500

    updating routes

--- TodoApp/src/main/java/com/sgz/TodoApp/controllers/TodoController.java
+++ TodoApp/src/main/java/com/sgz/TodoApp/controllers/TodoController.java
@@ -10,54 +10,49 @@ import com.sgz.TodoApp.exceptions.InvalidEntityException;
 import com.sgz.TodoApp.exceptions.InvalidIdException;
 import com.sgz.TodoApp.exceptions.NoItemsException;
 import com.sgz.TodoApp.services.TodoService;
-import java.util.List;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  *
  * @author samg.zun
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/todos")
 public class TodoController {
     
     @Autowired
     TodoService service;
     
-    @GetMapping("/todos")
+    @GetMapping
     public ResponseEntity<List<Todo>> getAll() throws NoItemsException{
         return ResponseEntity.ok(service.getAll());
     }
     
-    @GetMapping("/todo/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Todo> getById(@PathVariable int id) throws InvalidIdException {
         return ResponseEntity.ok(service.getById(id));
     }
     
-    @DeleteMapping("/todo/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity deleteById(@PathVariable int id) throws InvalidIdException {
         service.deleteTodo(id);
         return new ResponseEntity(HttpStatus.OK);
     }
     
-    @PostMapping("/todos")
+    @PostMapping
     public ResponseEntity<Todo> addTodo(@Valid @RequestBody Todo toAdd) throws InvalidEntityException{
         return new ResponseEntity(service.createTodo(toAdd), HttpStatus.CREATED);
     }
     
-    @PutMapping("/todos")
-    public ResponseEntity<Todo> updateTodo(@Valid @RequestBody Todo toAdd) throws InvalidEntityException, InvalidIdException{
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable int id, @Valid @RequestBody Todo toAdd) throws InvalidEntityException, InvalidIdException{
+        toAdd.setId(id);
         return new ResponseEntity(service.editTodo(toAdd), HttpStatus.OK);
     }
 }
