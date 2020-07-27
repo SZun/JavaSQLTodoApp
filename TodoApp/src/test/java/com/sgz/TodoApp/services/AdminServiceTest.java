bb9b70e2357a11a718babd29f7fc3c7f2b431ce8
Zun <zunsamuel97@gmail.com>
2020-07-26 23:02:09 -0500

    finished setting up test suite for admin service

--- TodoApp/src/test/java/com/sgz/TodoApp/services/AdminServiceTest.java
+++ TodoApp/src/test/java/com/sgz/TodoApp/services/AdminServiceTest.java
@@ -1,6 +1,7 @@
 package com.sgz.TodoApp.services;
 
 import com.google.common.collect.Sets;
+import com.sgz.TodoApp.TestAppConfig;
 import com.sgz.TodoApp.entities.ApplicationRole;
 import com.sgz.TodoApp.entities.ApplicationUser;
 import com.sgz.TodoApp.exceptions.InvalidEntityException;
@@ -8,13 +9,19 @@ import com.sgz.TodoApp.exceptions.InvalidNameException;
 import com.sgz.TodoApp.repos.UserRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Set;
 
 //import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = TestAppConfig.class)
 class AdminServiceTest {
 
     @Autowired
@@ -29,6 +36,9 @@ class AdminServiceTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     private final Set<ApplicationRole> testRoles = Sets.newHashSet(new ApplicationRole(1, "USER"));
 
     private final String testLongString = "C39V2iGLMtU1xN8tctQQVPnr7Y41mgIqCCPKookK7yrKP9xweAp6Oo7NGOBp6wkWIP1cQZvxW2n40ZK0vUUHWxQzhjUCRnUXFx1uSSKXYP37nlsLcMnmaxpnGY7JGmKap7Q4e1mdtVg3aZ829B3IeMCzxTs2Ex5IOrbgu55cwUKh3z7GBFssVQL4mzr1eHqfOv67prPQgcCQCDIRSEZH1tt0h5yxVgVt2prBdgUWBmo6sg6UPS6k1quBYGDoFBIk";
@@ -39,7 +49,7 @@ class AdminServiceTest {
         jdbc.update("DELETE FROM Roles");
         jdbc.update("ALTER TABLE Users auto_increment = 1");
         jdbc.update("ALTER TABLE Roles auto_increment = 1");
-        jdbc.update("INSERT INTO Roles(Authority) VALUES('USER')('ADMIN')");
+        jdbc.update("INSERT INTO Roles(Authority) VALUES('USER'),('ADMIN')");
         userService.createUser(new ApplicationUser(testRoles, "@amBam20","Sam"));
     }
 
