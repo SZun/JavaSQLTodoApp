8f5fe0e0f08461d32d660aec873a2cf34944ae92
Zun <zunsamuel97@gmail.com>
2020-07-31 15:10:55 -0500

    generated all service tests

---
+++ TodoApp/src/test/java/com/sgz/TodoApp/services/AuthServiceTest.java
@@ -0,0 +1,12 @@
+package com.sgz.TodoApp.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @Test
+    void getUserId() {
+    }
+}
---
+++ TodoApp/src/test/java/com/sgz/TodoApp/services/RoleServiceTest.java
@@ -0,0 +1,32 @@
+package com.sgz.TodoApp.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleServiceTest {
+
+    @Test
+    void getAllRoles() {
+    }
+
+    @Test
+    void getRoleById() {
+    }
+
+    @Test
+    void getRoleByAuthority() {
+    }
+
+    @Test
+    void createRole() {
+    }
+
+    @Test
+    void editRole() {
+    }
+
+    @Test
+    void deleteRoleById() {
+    }
+}
