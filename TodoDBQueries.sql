91f94fbff829283a7e49e6b58a8ca7fffe78d661
Zun <zunsamuel97@gmail.com>
2020-08-01 19:51:29 -0500

    updated sql scripts

--- TodoDBQueries.sql
+++ TodoDBQueries.sql
@@ -3,27 +3,23 @@ USE TodoDB;
 -- get all todos
 SELECT * FROM Todos;
 -- get one todo by id and user id
-SELECT * FROM Todos WHERE Id = 1 AND User_Id = 1;
+-- SELECT * FROM Todos WHERE Id =  AND User_Id = ;
 -- get one todo by id
-SELECT * FROM Todos WHERE Id = 1 ;
+-- SELECT * FROM Todos WHERE Id = ;
 
 -- get all users
 SELECT * FROM Users;
 -- get user by id
-SELECT * FROM Users WHERE Id = 1;
+-- SELECT * FROM Users WHERE Id = ;
 -- get all users with certain role by role id
-SELECT u .* FROM Users u 
-JOIN Users_Roles ur ON u.Id = ur.User_Id
-WHERE ur.Role_Id = 1;
+-- SELECT u .* FROM Users u JOIN Users_Roles ur ON u.Id = ur.User_Id WHERE ur.Role_Id = ;
 
 -- get all roles
 SELECT * FROM Roles;
 -- get role by id
-SELECT * FROM Roles WHERE Id = 1;
+-- SELECT * FROM Roles WHERE Id = ;
 -- get all roles with certain user by user id
-SELECT r.* FROM Roles r
-JOIN Users_Roles ur ON r.Id = ur.Role_Id
-WHERE ur.User_Id = 1;
+-- SELECT r.* FROM Roles r JOIN Users_Roles ur ON r.Id = ur.Role_Id WHERE ur.User_Id = ;
 
 -- get all Users_Roles
 SELECT * FROM Users_Roles;
--- TodoDBSeed.sql
+++ TodoDBSeed.sql
@@ -1,7 +1,6 @@
 USE TodoDB;
 
-INSERT INTO Users(Username, `Password`) VALUES('Zun','$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS');
-INSERT INTO Roles(Authority) VALUES('USER'),('ADMIN');
-INSERT INTO Users_Roles(User_Id,Role_Id) VALUES(1,1),(1,2);
-
-INSERT INTO Todos(`Name`,Start_Date,User_Id) VALUES('Walk Dog','2020-07-29',1);
+INSERT INTO Users(Id, Username, `Password`) VALUES('3eda4540-ffe3-4083-8cbd-78955baa81e6', 'Zun','$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS');
+INSERT INTO Roles(Id, Authority) VALUES('4656e253-8873-4a93-9340-cf1912051992', 'USER'),('2bd87c4e-39ac-41fd-854d-b480af2374e4','ADMIN');
+INSERT INTO Users_Roles(User_Id,Role_Id) VALUES('3eda4540-ffe3-4083-8cbd-78955baa81e6','4656e253-8873-4a93-9340-cf1912051992'),
+('3eda4540-ffe3-4083-8cbd-78955baa81e6','2bd87c4e-39ac-41fd-854d-b480af2374e4');
