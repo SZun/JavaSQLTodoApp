eacbcbca6c10a023d728ce398b5937c371889cdd
Zun <zunsamuel97@gmail.com>
2020-08-01 20:51:16 -0500

    removedd un-needed

--- TodoDBSeed.sql
+++
@@ -1,6 +0,0 @@
--- USE TodoDB;
-
--- INSERT INTO Users(Id, Username, `Password`) VALUES('3eda4540-ffe3-4083-8cbd-78955baa81e6', 'Zun','$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS');
--- INSERT INTO Roles(Id, Authority) VALUES('4656e253-8873-4a93-9340-cf1912051992', 'USER'),('2bd87c4e-39ac-41fd-854d-b480af2374e4','ADMIN');
--- INSERT INTO Users_Roles(User_Id,Role_Id) VALUES('3eda4540-ffe3-4083-8cbd-78955baa81e6','4656e253-8873-4a93-9340-cf1912051992'),
--- ('3eda4540-ffe3-4083-8cbd-78955baa81e6','2bd87c4e-39ac-41fd-854d-b480af2374e4');
